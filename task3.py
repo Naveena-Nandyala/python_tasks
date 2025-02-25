bf82838e076c3010c056ef7d2f30db4e4a1beff9
Naveena-Nandyala <naveenanavi.2029@gmail.com>
2025-02-25 16:47:10 +0530

    msg

--- task3.py
+++ task3.py
@@ -1,4 +1,4 @@
-# # 1. Print the first 10 terms of the Fibanocci series using a For loop:
+#  1. Print the first 10 terms of the Fibanocci series using a For loop:
 
 terms =int(input("Enter a number"))
 num1 = 0
@@ -10,7 +10,7 @@ for i in range(2, terms):
     num1 = num2
     num2 = num3
 
-# # 2. Check if a given number is a prime number using a For loop:
+#  2. Check if a given number is a prime number using a For loop:
 
 num1 = int(input("Enter a number to check prime or not"))
 flag = True
@@ -23,7 +23,7 @@ for i in range(2, num1):
 if flag:
     print(num1, "is a Prime Number")
 
-# # 3. Write a Program to calculate the factorial of a number using a For Loop:
+#  3. Write a Program to calculate the factorial of a number using a For Loop:
 
 num = int(input("Enter a number to calculate the factorial:"))
 res = 1
@@ -32,16 +32,16 @@ while num > 1:
     num -= 1
 print("The factorial is:", res)
 
-# # 4. Print all numbers from 1 to 100 that are divisible by 3 and 5 using a For Loop:
+#  4. Print all numbers from 1 to 100 that are divisible by 3 and 5 using a For Loop:
 
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print(i, end=" ")
 
-# # 5. Implement a menu - driven program when the user can choose to:
-# #    1. Find the square of a number
-# #    2. Find the cube of a number
-# #    3. Exit
+# 5. Implement a menu - driven program when the user can choose to:
+#     1. Find the square of a number
+#     2. Find the cube of a number
+#     3. Exit
 
 num = int(input("Enter a Number"))
 choice  = int(input("Enter a number from 1 to 3"))
